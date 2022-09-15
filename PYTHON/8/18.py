9e8e4f16a99b70f129d8b0af719024a749999df8
SM-2102 <91632338+SM-2102@users.noreply.github.com>
2022-09-15 10:57:55 +0530

    R

--- PYTHON/8/18.py
+++ PYTHON/8/18.py
@@ -2,11 +2,11 @@
 # create another dictionary from this dictionary that has name of the students and their total marks
 # find out the topper and his/her score
 
-dict = {'Aranya' : [10,10,10,10],'Koyena' : [10,10,10,9],'Dipan':[10,10,10,8]}
+dict = {'Aranya' : [10,10,10,9],'Koyena' : [10,10,10,10],'Dipan':[10,10,10,8]}
 dict2 = {}
 for key,value in dict.items():
     dict2[key] = sum(value)
 maxi = max(dict2.values())
 for key,value in dict2.items():
     if value == maxi:
-        print('The topper is',key)
+        print('The topper is',key,'with',maxi,'marks.')
