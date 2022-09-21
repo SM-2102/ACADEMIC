91fd389721c498644f5ae50c756b796417e2d983
SM-2102 <sukanya.manna.2002@gmail.com>
2022-09-21 22:39:13 +0530

    yo

--- PYTHON/9/6.py
+++ PYTHON/9/6.py
@@ -9,7 +9,7 @@ def ncr(n,r):
    return int(fact(n)/(fact(n-r) *fact(r)))
 
 n = int(input('Enter the number of rows : '))
-for i in range(0,n+1):
+for i in range(0,n):
     for j in range(0,n-i+1):
         print(" ",end='')
     for j in range(0,i+1):
