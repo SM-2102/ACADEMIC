365b0dd41a944595e0fdd1c51752642e06cdc6e7
SM-2102 <sukanya.manna.2002@gmail.com>
2022-10-27 15:46:06 +0530

    mo

--- PYTHON/11/9.py
+++ PYTHON/11/9.py
@@ -1,8 +1,7 @@
 # count the number of words in file
 
-words=0
 with open('file1.txt','r') as file:
     data = file.read()
     lines = data.split()
-    words= words+ len(lines)
+    words=len(lines)
 print('The number of words :',words)
