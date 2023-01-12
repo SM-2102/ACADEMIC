e9486a8aecb699c9478e7b2bf4542080eb59f236
SM-2102 <sukanya.manna.2002@gmail.com>
2023-01-12 21:22:07 +0530

    df

--- DSA/19/hashing.c
+++ DSA/19/hashing.c
@@ -4,75 +4,79 @@
     3. mid square method
     4. folding method */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 
-#define m 100
-
-char result[100];
-
-struct node
+int division(int key,int size)
 {
-    int key;
-    struct node *next;
-};
-
-int division(int key)
-{
-    return key%m;
+	return key%size;
 }
 
-int multiplication(int key)
+int multiplication(int key, int size)
 {
     float a = (sqrt(5)-1)/2;
-    float i = m*((key*a)-(int)(key*a));
+    float i = size*((key*a)-(int)(key*a));
     int h = floor(i);
     return h;
 }
 
-struct node *table[m] = {NULL};
-
-void insert(int i,int key)
+int mid_sq(int key, int size)
 {
-    struct node *new_node = (struct node *)malloc(sizeof(struct node));
-    struct node *ptr;
-    new_node->key = key;
-    new_node->next = NULL;
-
-    if(table[i]==NULL)
-        table[i] = new_node;
-    else
+	int ks=0,ss=0,k2=key*key,t=k2;
+	while(t>0)
     {
-        ptr = table[i];
-        while(ptr->next!=NULL)
-            ptr = ptr->next;
-        ptr->next = new_node;
-    }
+		t/=10;
+		ks+=1;
+	}
+	t=size-1;
+	ks/=2;
+	while(t>0)
+    {
+		t/=10;
+		ss+=1;
+	}
+	int f=(int)pow(10,ks+ss-1),r=(int)pow(10,ks-ss/2);
+	return ((k2%f)/r)%size;
 }
 
-void print()
+int folding(int key, int size)
 {
-    struct node *ptr;
-    for(int i=0;i<m;i++)
+	int ss=0,t=size-1,rem=0,sum=0;
+	while(t>0)
     {
-        printf("%d",i);
-        if (table[i] == NULL)
-            printf(" -> NULL");
-        else
-        {
-            for(ptr=table[i];ptr!=NULL;ptr=ptr->next)
-                printf(" -> %d",ptr->key);
-        }
-        printf("\n");
-    }
+		t/=10;
+		ss++;
+	}
+	t=key/ss;
+	rem=key%ss;
+	ss=(int)pow(10,ss);
+	sum+=key%(int)pow(10,rem);
+	key/=(int)pow(10,rem);
+	while(t>0)
+    {
+		sum+=key%ss;
+		key/=ss;
+		t--;
+	}
+	return (sum%ss)%size;
 }
 
-int main()
+void print(int arr[],int size)
 {
-    int choice,key,i;
-    printf("Enter :\n1 for division method\n2 for multiplication method\n3 for mid square method\n4 for folding method\n5 to print the hash table\n6 to exit.\n");
+    for(int i=0;i<size;i++)
+        printf("%d -> %d\n",i,arr[i]);
+
+}
+
+void main(){
+	int size, key, choice,index, temp;
+	printf("Enter size of hash table : ");
+	scanf("%d",&size);
+	int arr[size];
+	for(int i=0;i<size;i++)
+		arr[i]=-1;
+	printf("Enter :\n1 for division method\n2 for multiplication method\n3 for mid square method\n4 for folding method\n5 to print the hash table\n6 to exit.\n");
     do
     {
         printf("Enter choice : ");
@@ -85,28 +89,40 @@ int main()
         switch(choice)
         {
             case 1: 
-                i = division(key);
-                insert(i,key);
+                index = division(key,size);
                 break;
             case 2:
-                i = multiplication(key);
-                insert(i,key);                
+                index = multiplication(key,size) ;             
                 break;
             case 3:
-                i = midsquare(key,2);
-                insert(i,key);
+                index = mid_sq(key,size);
                 break;
             case 4:
-                i = folding(key,2);
-                insert(i,key);
+                index = folding(key,size);
                 break;
             case 5:
-                print();
-                break;
+                print(arr,size);
+                continue;
             case 6:
-                break;
+                exit(0);
             default:
                 printf("Invalid choice.\n");
+                continue;
         }
+        temp = index;
+        do
+        {
+			if(arr[index]==-1)
+            {
+				arr[index]=key;
+				printf("Key %d placed at index %d\n",key,index);
+				temp=-1;
+				break;
+			}
+			index=(index+1)%size;
+		}while(temp!=index);
+
+		if(temp==index)
+			printf("Overflow\n");
     } while (choice!=6);
 }
