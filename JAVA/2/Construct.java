8dd7f9d9ca8eebdfe9347c8eb5d43114f50c6c6e
SM-2102 <sukanya.manna.2002@gmail.com>
2023-07-28 13:45:43 +0530

    Assignment 2

---
+++ JAVA/2/Construct.java
@@ -0,0 +1,16 @@
+class Construct
+{
+	int width,height,depth;
+	Construct()
+	{
+		width =10;
+		height=15;
+		depth=20;
+	}
+
+	public static void main(String args[])
+	{	
+		Construct mybox = new Construct();
+		mybox.Construct();
+		
+		
