3b12dd8a3de60e5e0e5267c5b684f1c0a7615c59
SM-2102 <sukanya.manna.2002@gmail.com>
2023-08-24 01:42:11 +0530

    Assignment 4

--- OS/2/10.sh
+++ OS/2/10.sh
@@ -22,44 +22,30 @@ delete_student()
 
 add_student() 
 {
-    echo "Enter roll number, name, and city :"
+    echo "Enter roll number, name and city :"
     read roll_no name city
     echo "$roll_no $name $city" >> "$filename"
-    echo "Student entry added successfully."
 }
 
 update_student() 
 {
-    echo "Enter roll number, name, and city :"
+    echo "Enter roll number, name and city :"
     read roll_no name city
     sed -i "/^$roll_no /s/[^ ]* [^ ]* [^ ]*/$roll_no $name $city/" "$filename"
-    echo "Student entry updated successfully."
 }
 
 while true; 
 do
-    echo "Menu:"
-    echo "1. Display entire contents of the file"
-    echo "2. Display student info by roll number"
-    echo "3. Delete student entry by roll number"
-    echo "4. Add a new student entry"
-    echo "5. Update student entry"
-    echo "6. Exit"
+    echo -e "Enter :\n1. Display entire contents of the file\n2. Display student info by roll number\n3. Delete student entry by roll number\n4. Add a new student entry\n5. Update student entry\n6. Exit"
     read choice
 
     case $choice in
-        1) display_file 
-        ;;
-        2) display_student 
-        ;;
-        3) delete_student 
-        ;;
-        4) add_student 
-        ;;
-        5) update_student 
-        ;;
-        6) break 
-        ;;
-        *) echo "Invalid choice. Please select a valid option." ;;
+        1) display_file ;;
+        2) display_student ;;
+        3) delete_student ;;
+        4) add_student ;;
+        5) update_student ;;
+        6) break ;;
+        *) echo "Invalid choice." ;;
     esac
 done
