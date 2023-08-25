447923a523b98ed750b755a65ffcb70c02d711ed
SM-2102 <sukanya.manna.2002@gmail.com>
2023-08-25 13:34:00 +0530

    Assignment 6

--- JAVA/6/TwoDFigure.java
+++ JAVA/6/TwoDFigure.java
@@ -1,5 +1,44 @@
 abstract class TwoDFigure
 {
-    double dim1, dim2, dim3;
-    
+    double dim1, dim2;
+    TwoDFigure(double a, double b)
+    {
+        dim1=a;
+        dim2=b;
+    }
+    void calc_area();
+}
+
+class Rectangle extends TwoDFigure
+{
+    Rectangle(double height, double width)
+    {
+        super(height,width);
+    }
+    void calc_area()
+    {
+        double area = dim1*dim2;
+        System.out.println("The area of rectangle : "+area);
+    }
+}
+
+class Triangle extends TwoDFigure
+{
+    Triangle(double height, double base)
+    {
+        super(height,base);
+    }
+    void calc_area()
+    {
+        double area = (dim1*dim2)/2;
+        System.out.println("The area of triangle : "+area);
+    }
+}
+
+class Mainclass
+{
+    public static void main(String args[])
+    {
+        
+    }
 }
