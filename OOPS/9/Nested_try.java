04778495726f2479e9a20525366f7dd7b156f715
SM-2102 <sukanya.manna.2002@gmail.com>
2023-09-21 22:16:08 +0530

    Assignment done

---
+++ OOPS/9/Nested_try.java
@@ -0,0 +1,22 @@
+class Nested_try
+{
+	public static void main(String args[])
+    {
+		try
+        {
+			try
+            {
+				String s= args[0];
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+            {
+				System.out.println("Array out of bound Exception");
+			    int a=5/0;
+			}
+		}	
+		catch(ArithmeticException e)
+        {
+			System.out.println("Can not Divided by Zero");
+		}	
+	}
+}
