ba4d68e6c1ded9239c0b502c0467a018b42de0fc
SM-2102 <sukanya.manna.2002@gmail.com>
2023-10-14 00:01:00 +0530

    Assigment 10 done

--- OS/5/execlt.c
+++ OS/5/execlt.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<unistd.h>
 
-int main(){
+int main()
+{
+	char *const evp[] = {"PATH=/bin:/usr/bin","TERM:console",0};
 	printf("Before execl\n");
 	execl("/bin/ps","ps","-a",NULL);
 	printf("After execlp\n");
