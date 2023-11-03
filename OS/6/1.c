9aa4af246cf13cc64575aa95c3980b1516972a7f
SM-2102 <91632338+SM-2102@users.noreply.github.com>
2023-11-03 12:16:07 +0530

    Update 1.c

--- OS/6/1.c
+++ OS/6/1.c
@@ -1,26 +1,36 @@
-#include <signal.h>
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
 
-int i=2;
-
-void oh(int sig)
-{
-    printf("Pressed Ctrl+C\n");
-    i--;
-    signal(SIGINT,oh);
-    if(i==0)
-    {
-        printf("Restoring to default.\n");
-        signal(SIGINT,SIG_DFL);
-    }
-}
-int main()
-{
-    signal(SIGINT, oh);
-    while(1)
-    {
-        printf("Hello World!\n");
-        sleep(1);
-    }
+int main(){
+	pid_t ppid, pid, cpid;
+	ppid = getpid();
+	pid = fork();
+	if(ppid == getpid())
+		printf("Parent\n");
+	else if(cpid == getpid())
+		printf("Child\n");
+	if(pid > 0){
+		int i = 0;
+		while(i++ < 5){
+			printf("In the parent process.\n");
+			sleep(1);
+		}
+	}
+	else if(pid == 0){
+		int i = 0;
+		while(i++ < 10){
+			printf("In the child process.\n");
+			sleep(1);
+			if(i==3){
+				kill(ppid, SIGKILL);
+				printf("Parent killed. I'm orphan!!!\n");
+			}
+		}
+	}
+	else{
+		printf("Something bad happened\n");
+		exit(EXIT_FAILURE);
+	}
 }
