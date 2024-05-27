9318dd6c116b1378e674ece401fa341cd9c392ba
SM-2102 <sukanya.manna.2002@gmail.com>
2024-05-27 23:35:29 +0530

    .

--- CN/7/client.py
+++ CN/7/client.py
@@ -4,7 +4,7 @@ server_ip = '127.0.0.1'
 server_port = 12345
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_ip, server_port))
+client_socket.connect(server_ip, server_port)
 
 message = "Hello, Echo Server!"
 
