0acec42150f9628358284cdf0ae0da0081e27d3a
SM-2102 <sukanya.manna.2002@gmail.com>
2024-05-28 14:07:40 +0530

    uppercase

---
+++ CN/9/server.py
@@ -0,0 +1,19 @@
+import socket
+
+server_ip = '127.0.0.1'
+server_port = 12345
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((server_ip, server_port))
+server_socket.listen()
+print("Server is listening on", server_ip, "port", server_port)
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print("Client connected from", client_address)
+
+    lowercase_string = client_socket.recv(1024).decode()
+    uppercase_string = lowercase_string.upper()
+    client_socket.sendall(uppercase_string.encode())
+
+    client_socket.close()
