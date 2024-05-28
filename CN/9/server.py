eb91f989446df684baccfa04b85a4c0258604840
SM-2102 <sukanya.manna.2002@gmail.com>
2024-05-28 16:38:12 +0530

    .

--- CN/9/server.py
+++ CN/9/server.py
@@ -4,7 +4,7 @@ server_ip = '127.0.0.1'
 server_port = 12345
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_ip, server_port))
+server_socket.bind(server_ip, server_port)
 server_socket.listen()
 print("Server is listening on", server_ip, "port", server_port)
 
